79175cadfcce6d7da0506173a38e368d7a99c021
andreyshishkov976 <andreyshishkov976@hotmail.com>
2022-07-05 11:51:05 +0300

    IdentityServer has been configured fo m2m communication

--- WebMart.IdentityServer/Config.cs
+++ WebMart.IdentityServer/Config.cs
@@ -1,9 +1,17 @@
+using IdentityServer4;
 using IdentityServer4.Models;
 
 namespace WebMart.IdentityServer
 {
     public static class Config
     {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
         public static IEnumerable<ApiScope> ApiScopes =>
             new List<ApiScope>
             {
@@ -13,27 +21,48 @@ namespace WebMart.IdentityServer
             };
 
         public static IEnumerable<Client> Clients =>
-            new List<Client>
+            new Client[]
             {
+                // m2m client credentials flow client
                 new Client
                 {
-                    ClientId = "BasketService.Client",
+                    ClientId = "m2m.client",
+                    ClientName = "ClientCredentials.Client",
+
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets =
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                    AllowedScopes =
                     {
-                        new Secret("BasketService.Secret".Sha256())
-                    },
-                    AllowedScopes = { "CatalogService" }
+                        "CatalogService",
+                        "BasketService",
+                        "OrderService"
+                    }
                 },
+
+                // interactive client using code flow
                 new Client
                 {
-                    ClientId = "OrderService.Client",
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets =
+                    ClientId = "interactive.client",
+                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RedirectUris = { "https://localhost:44300/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    RequirePkce = false,
+                    AllowedScopes =
                     {
-                        new Secret("OrderService.Secret".Sha256())
-                    },
-                    AllowedScopes = { "BasketService" }
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
+                        "CatalogService",
+                        "BasketService",
+                        "OrderService"
+                    }
                 },
             };
     }
--- WebMart.IdentityServer/Program.cs
+++ WebMart.IdentityServer/Program.cs
@@ -2,36 +2,15 @@ using WebMart.IdentityServer;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var isBuilder = builder.Services.AddIdentityServer()
+    .AddInMemoryIdentityResources(Config.IdentityResources)
     .AddInMemoryApiScopes(Config.ApiScopes)
     .AddInMemoryClients(Config.Clients);
 
 isBuilder.AddDeveloperSigningCredential();
 
-
-// builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-
-// app.UseHttpsRedirection();
-
-app.UseDeveloperExceptionPage();
-
 app.UseIdentityServer();
-
-// app.UseAuthorization();
-
-// app.MapControllers();
 
 app.Run();
